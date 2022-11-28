1a0a168c4848aa8f459b84c6dc5762d0b36d4770
hoang.le <leehoang@gmail.com>
2022-11-28 08:21:19 +0700

    doi

--- main.js
+++ main.js
@@ -1,26 +1,9 @@
-let canvas = document.getElementById('gamezone');
-let context = canvas.getContext('2d');
-let diem = document.getElementById('d');
-
-let hulkimg = new Image();
-hulkimg.src = "anh/hulk1.png";
-
-let nen = new Image()
-nen = "anh/nenchinh.png";
-
-let quan = new Image()
-quan = "anh/quan.png";
-
-
-let score=0;
-let hulk={
-    x: nen.width/2.5,
-    y: nen.high/5,
+let a= document.getElementById('gamezone');
+let ctx=a.getContext('2d');
+let img= document.getElementById('anh');
+function draw() {ctx.drawImage(img, 150, 500, 90, 90)
 }
 
-function run(){
-    context.drawImage(nen,0,0);
-    context.drawImage(hulkimg,hulk.x,hulk.y);
-    requestAnimationFrame(run)
-}
-run()
+
+
+
